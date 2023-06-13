187a073353cd8c10ad70849b84edad0c245510cb
tfvenu2023 <136424675+tfvenu2023@users.noreply.github.com>
2023-06-13 13:12:02 +0530

    backend

--- providers.tf
+++ providers.tf
@@ -5,7 +5,15 @@ terraform {
       version = "3.60.0"
     }
   }
+  backend "azurerm" {
+    resource_group_name  = "StorageAccount-ResourceGroup"
+    storage_account_name = "abcd1234"
+    container_name       = "tfstate"
+    key                  = "prod.terraform.tfstate"
+  }
 }
+
+
 
 provider "azurerm" {
   # Configuration options
